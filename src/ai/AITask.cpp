409bb8fe8322f0dd453d791c093fdb1106c2cf76
Raphael Isemann <teemperor@gmail.com>
2018-09-15 23:09:59 +0200

    Fixed clang warnings about emitting vtables

--- src/ai/AITask.cpp
+++ src/ai/AITask.cpp
@@ -24,26 +24,28 @@ public:
   WalkTask(TilePos Target) : Target(Target) {
   }
 
-  AITask *act(Character &C, float DTime) override {
-    if (WalkPath.empty()) {
+  AITask *act(Character &C, float DTime) override;
+};
+
+AITask *WalkTask::act(Character &C, float DTime) {
+  if (WalkPath.empty()) {
       PathFinder finder(C.getLevel());
       finder.findPath(C.getTilePos(), Target, WalkPath);
     }
 
-    if (WalkPath.empty())
-      return finish("Couldn't find a path");
+  if (WalkPath.empty())
+    return finish("Couldn't find a path");
 
-    if (Vec2(WalkPath.back()).distance(C.getPos()) < 5.0f)
-      WalkPath.pop_back();
+  if (Vec2(WalkPath.back()).distance(C.getPos()) < 5.0f)
+    WalkPath.pop_back();
 
-    if (WalkPath.empty())
-      return finish("Finished path");
+  if (WalkPath.empty())
+    return finish("Finished path");
 
-    C.walkToward(WalkPath.back(), DTime);
+  C.walkToward(WalkPath.back(), DTime);
 
-    return nullptr;
-  }
-};
+  return nullptr;
+}
 
 class CombatTask : public AITask {
   Creature *Target;
@@ -51,22 +53,24 @@ public:
   CombatTask(Creature *Target) : Target(Target) {
   }
 
-  AITask *act(Character &C, float DTime) override {
-    if (C.isDead())
-      return finish("Target dead");
-
-    const Item &weapon = C.getEquipped(ItemData::Weapon);
-
-    if (C.getPos().distance(Target->getPos()) > weapon.getRange())
-      return finish("Enemy too far away");
-
-    if (&Target->getLevel() != &C.getLevel())
-      return finish("Enemey in different level");
-
-    C.tryShootAt(*Target);
-    return nullptr;
-  }
+  AITask *act(Character &C, float DTime) override;
 };
+
+AITask *CombatTask::act(Character &C, float DTime) {
+  if (C.isDead())
+    return finish("Target dead");
+
+  const Item &weapon = C.getEquipped(ItemData::Weapon);
+
+  if (C.getPos().distance(Target->getPos()) > weapon.getRange())
+    return finish("Enemy too far away");
+
+  if (&Target->getLevel() != &C.getLevel())
+    return finish("Enemey in different level");
+
+  C.tryShootAt(*Target);
+  return nullptr;
+}
 
 class HuntTask : public AITask {
   Creature *Target;
@@ -75,38 +79,44 @@ class HuntTask : public AITask {
 public:
   HuntTask(Creature *Target) : Target(Target) {
   }
-  AITask *act(Character &C, float DTime) override {
-    if (C.isDead())
-      return finish("Target is dead");
-
-    InCombat = false;
-
-    const Item &weapon = C.getEquipped(ItemData::Weapon);
-    WeaponRange = weapon.getRange();
-
-    if (C.getPos().distance(Target->getPos()) > WeaponRange) {
-      return new WalkTask(Target->getTilePos());
-    } else {
-      InCombat = true;
-      return new CombatTask(Target);
-    }
-  }
+  AITask *act(Character &C, float DTime) override;
 
   bool actInactive(Character &C) override {
     return !InCombat && C.getPos().distance(Target->getPos()) < WeaponRange;
   }
 };
 
+AITask *HuntTask::act(Character &C, float DTime) {
+  if (C.isDead())
+    return finish("Target is dead");
+
+  InCombat = false;
+
+  const Item &weapon = C.getEquipped(ItemData::Weapon);
+  WeaponRange = weapon.getRange();
+
+  if (C.getPos().distance(Target->getPos()) > WeaponRange) {
+      return new WalkTask(Target->getTilePos());
+  } else {
+    InCombat = true;
+    return new CombatTask(Target);
+  }
+}
+
+
 class MainTask : public AITask {
 public:
-  AITask *act(Character &C, float DTime) override {
-    auto ClosestEnemies = C.getClosestEnemies(16 * 20);
-    if (!ClosestEnemies.empty()) {
+  AITask *act(Character &C, float DTime) override;
+};
+
+AITask *MainTask::act(Character &C, float DTime) {
+  auto ClosestEnemies = C.getClosestEnemies(16 * 20);
+  if (!ClosestEnemies.empty()) {
       return new HuntTask(ClosestEnemies.front());
     }
-    return nullptr;
-  }
-};
+  return nullptr;
+}
+
 
 void CharacterAI::popBack() {
   delete Tasks.back();
